a4f9bea06671951d7b88d522bb2ffac8154e9639
m0nq <forgetmenaught@gmail.com>
2024-01-17 11:12:36 -0800

    Updated Pet component with more info and better styling

---
+++ src/Results.jsx
@@ -0,0 +1,22 @@
+import Pet from './Pet';
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No pets found</h1>
+      ) : (
+        pets.map(({name, animal, id, breed, images, city, state }) => (
+          <Pet key={id}
+            name={name}
+            animal={animal}
+            breed={breed}
+            images={images}
+            location={`${city}, ${state}`}/>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
