cae4b1cf2c7b05fab8b6a1b2e7474164020f18b5
m0nq <forgetmenaught@gmail.com>
2024-01-22 12:05:51 -0800

    Create class component for Carousel

---
+++ src/Carousel.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+export default class Carousel extends Component {
+  state = {
+    active: 0
+  };
+
+  static defaultProps = {
+    images: ['https://pets-images.dev-apis.com/pets/none.jpg']
+  };
+
+  handleIndexClick = e => {
+    this.setState({
+      active: +e.target.dataset.index
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero"/>
+        <div className="carousel-smaller">
+          {images.map((photo, i) => {
+            return (
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
+              <img onClick={this.handleIndexClick}
+                data-index={i}
+                key={photo}
+                src={photo}
+                className={i === active ? 'active' : ''}
+                alt="animal thumbnail"/>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
