6fe45d1f2a679557caa8e2f593b117d0a054a600
m0nq <forgetmenaught@gmail.com>
2024-01-22 12:05:51 -0800

    Create class component for Carousel

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
+import Carousel from './Carousel';
+
 import fetchPet from './fetchPet';
 
 const Details = () => {
@@ -19,20 +21,12 @@ const Details = () => {
     return <div className="loading-pane">There was an error. Please refresh and try again...</div>;
   }
 
-  console.log('results.data ->', results.data);
   const pet = results.data?.pets[0];
 
   return (
     <div className="details">
+      <Carousel images={pet.images}/>
       <div>
-        <img src={pet.images?.[id]}
-          alt={`${pet.breed} ${pet.animal}`}
-          style={{
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            width: '50%'
-          }}/>
         <h1>{pet.name}</h1>
         <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state}</h2>
       </div>
